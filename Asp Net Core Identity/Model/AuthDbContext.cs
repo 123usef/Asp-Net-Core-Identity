6545d23ac04e91b5f2663b243bbfc7e30af88659
YOUSF <YOUSF@pc>
2022-05-28 09:20:01 +0400

    Add project files.

---
+++ Asp Net Core Identity/Model/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp_Net_Core_Identity.Model
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Asp Net Core Identity/ViewModels/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp_Net_Core_Identity.ViewModels
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public String Email  { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        public bool Remmemberme { get; set; }
+    }
+}
